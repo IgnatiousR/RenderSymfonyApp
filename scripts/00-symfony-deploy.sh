808f4816c07220e78bb36d85946aa6841c147905
Ignatious <ignatiousr80@gmail.com>
2025-06-10 19:52:20 +0600

    Update 00-symfony-deploy.sh

--- scripts/00-symfony-deploy.sh
+++ scripts/00-symfony-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --no-dev --optimize-autoloader
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+
 # Warm up cache
 php bin/console cache:warmup
 echo "Caching config..."
