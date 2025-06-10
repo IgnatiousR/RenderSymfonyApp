ffd4a1c58775289f8068b65830acf93be800fb5e
Ignatious <ignatiousr80@gmail.com>
2025-06-10 18:01:41 +0600

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
